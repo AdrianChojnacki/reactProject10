7379c4a4b6653659619ac7cc207322b76193499d
Adrian Chojnacki <chojnackiadrian1@gmail.com>
2021-04-18 13:40:37 +0200

    zero fix

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ class Clock extends React.Component {
 
     return (
       <div>
-        {hours}:{minutes}:{seconds}
+        {hours < 10 ? `0${hours}` : hours}:{minutes < 10 ? `0${minutes}` : minutes}:
+        {seconds < 10 ? `0${seconds}` : seconds}
       </div>
     );
   }
